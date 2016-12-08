5c2a8ec8b15eeadd40a4866496082b19a6c00ba6
marcog77 <marco.girardello@gmail.com>
2016-12-08 00:36:05 -0100

    v1

--- R/imprrt.R
+++ R/imprrt.R
@@ -1,5 +1,6 @@
-#' Calculate variable importance for rrt models
 #'
+#' Calculate variable importance for rrt models
+#' 
 #' @ The function takes the following arguments
 #' @ intable=table from model selection results 
 #' @ index=model selection index to be used i.e. BIC or AIC 
