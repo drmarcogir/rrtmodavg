f12a5d9d71ee54ece00551e65b87bf1b78ed10e5
marcog77 <marco.girardello@gmail.com>
2017-01-16 16:31:19 +0200

    v1

---
+++ R/ts_get.R
@@ -0,0 +1,24 @@
+######################################################################
+#'  @ Function for retrieving a table of standard errors from rrt
+#'  @ models
+#'  @ The function takes the following arguments
+#'  @ intable=table from model selection results 
+#'####################################################################
+ts_get<-function(intable){
+    ml<-as.character(intable$modID)
+    mod.l<-get(load("models"))
+    resml<-NULL
+    for (m in 1:length(ml)){
+        mymod<-mod.l[ml[m]] # get model
+        options(warn = -1)
+        sest<-summary(mymod[[1]])$coefficients[,3] # summary with coefficients ALL coefficients here
+        if(length(sest)==0){
+            next
+        } 
+        resdf<-data.frame(modID=ml[m],varname=names(sest),se=sest)
+        resml<-rbind(resdf,resml)
+    }
+    results<-spread(data=resml,key=varname,value=se)
+    return(results)
+}
+
