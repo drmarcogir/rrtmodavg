0c48dd982033e854dbd8dab656d2b02e6d40f352
marcog77 <marco.girardello@gmail.com>
2017-02-09 21:23:33 +0100

    v1

--- R/predrrt_rep.R
+++ R/predrrt_rep.R
@@ -9,7 +9,7 @@
 
 
 predrrt_rep<-function(intable=NULL,index=NULL,modfilt=NULL,runid=NULL){
-    mod.l<-get(load("models"))
+    mod.l<-get(load("predictionsintable"))
     results<-NULL                  # store results of predictions
     iter.l<-unique(intable[,runid])  # list ID for iterations
     for (x in 1:length(iter.l)){      # loop through randomized model iterations 
@@ -46,26 +46,42 @@ predrrt_rep<-function(intable=NULL,index=NULL,modfilt=NULL,runid=NULL){
         options(warn = -0)
         # bind results together
         preddf<-do.call("rbind",pred.vec)
-        modid.l<-unique(preddf$modID)
-        preddf1<-NULL
-        for(b in 1:length(modid.l)){
-            tmp1<-subset(preddf,modID==modid.l[b])
-            tmp1$siteid<-1:dim(tmp1)[1]
-            preddf1<-rbind(tmp1,preddf1)
-        }
+        # modid.l<-unique(preddf$modID)
+        #preddf1<-NULL
+        #for(b in 1:length(modid.l)){
+         #   tmp1<-subset(preddf,modID==modid.l[b])
+       #     tmp1$siteid<-1:dim(tmp1)[1]
+        #    preddf1<-rbind(tmp1,preddf1)
+      #  }
         
-        preddf2<-merge(preddf1,tmp[,c("modID","weight")])
+        preddf2<-merge(preddf,tmp[,c("modID","weight")])
         site.l<-unique(preddf2$siteid)
         finalpred<-NULL
         for(m in 1:length(site.l)){
             tmp2<-subset(preddf2,siteid==site.l[m])
-            p<-weighted.mean(tmp2$pred,tmp2$weight)         
-            finalpred<-rbind(data.frame(p,siteid=site.l[m],iteration=x),finalpred)
+            p<-weighted.mean(tmp2$pred,tmp2$weight)
+            tmp3<-data.frame(p,siteid=site.l[m],iteration=x)
+            finalpred<-rbind(tmp3,finalpred)
         }
         
         results<-rbind(finalpred,results)
         
     }
-    results1<-aggregate(p~siteid,data=results,FUN=mean)
+    iter.l<-unique(results$iteration)
+    newsite<-NULL
+    for (i in 1:length(iter.l)){
+        tmpa1<-subset(results,iteration==iter.l[i])
+        newsite<-rbind(tmpa1,newsite)
+    }
+        
+    results2<-aggregate(p~siteid,data=results,FUN=mean)
+    tmpres<-unique(results2[1])
+    lookup<-data.frame(siteid=1:353)
+    missing<-anti_join(lookup,tmpres)
+    missing1<-data.frame(missing,p=NA)
+    results3<-rbind(results2,missing1)
+    results4<-arrange(results3,siteid)
+
+
     return(results1)
 }
